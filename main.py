f5581ad2a204c7036f2988b437859b28518731c6
ABeack24 <818639@mystma.org>
2023-03-29 14:45:46 -0500

    3

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 #WRITE YOUR CODE IN THIS FILE
 def hasL(x):
+    numL = 0
     for i in range (0, len(x)):
         if x[i] == "l":
-            return True
-        else:
-            return False
+            numL = numL + 1
+            if numL >= 1:
+             return True
+            else:
+                return False
+
+print(hasL("alabama"))
